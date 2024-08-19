ce91a6879c642c886de0a07f61716a95850530b0
Soft-Queen <taiwo.r@klakpad.com>
2024-08-19 09:53:20 +0100

    added the search input.

--- src/components/users.tsx
+++ src/components/users.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers, setSelectedUser, selectUsers, selectLoading } from "../redux/reducers/userSlice";
 import { Link, Outlet } from "react-router-dom";
@@ -9,6 +9,7 @@ const Users = () => {
   const dispatch = useDispatch<AppDispatch>();
   const users = useSelector(selectUsers);
   const loading = useSelector(selectLoading);
+  const [searchQuery, setSearchQuery] = useState<string>('');
 
   useEffect(() => {
     dispatch(fetchUsers());
@@ -18,15 +19,27 @@ const Users = () => {
     dispatch(setSelectedUser(user));
   };
 
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value);
+  };
+
+ 
+  const filteredUsers = users.filter(user =>
+    user.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+    user.email.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  
   return (
     <div className="container py-5">
       <div className="row">
         <h3 className="mb-5">List of Available Users</h3>
         {loading && <p>Loading...</p>}
           <div className="col-md-8 border-end pe-3">
+            <input type="text" onChange={handleSearch} className="form-control mb-3 py-2 shadow-none"/>
         <div className="row">
-          {users.length > 0 ? (
-            users.map((val: any) => (
+          {filteredUsers.length > 0 ? (
+            filteredUsers.map((val: any) => (
               <div className="col-md-4 mb-3" key={val.id}>
                 <Link to={`details/${val.id}`} className="text-decoration-none">
                 <div className="card p-4 h-100" onClick={() => handleUserDetails(val)}>
